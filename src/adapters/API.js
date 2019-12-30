98e6cd3285ca14c4bb214ca3d20109a2bd7ef586
github username <github email address>
2019-12-30 13:49:50 +0000

    urls working

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,5 +1,5 @@
 const API_ENDPOINT =
-  process.env.REACT_APP_API_ENDPOINT || "http://localhost:3000/api/v1";
+  process.env.REACT_APP_API_ENDPOINT || "http://localhost:3000/api/v1/";
 const LOGIN_URL = `${API_ENDPOINT}login`;
 const SIGNUP_URL = `${API_ENDPOINT}users`;
 const VALIDATE_URL = `${API_ENDPOINT}validate`;
--- src/components/UserItems.js
+++ src/components/UserItems.js
@@ -4,7 +4,7 @@ import Input from "./Input";
 import Button from "./Button.js";
 import { useHistory } from "react-router-dom";
 const API_ENDPOINT =
-  process.env.REACT_APP_API_ENDPOINT || "http://localhost:3000/api/v1";
+  process.env.REACT_APP_API_ENDPOINT || "http://localhost:3000/api/v1/";
 
 const UserItems = ({ user, setUser }) => {
   let history = useHistory();
